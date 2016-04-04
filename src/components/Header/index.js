249f58456deb713d5631fa977c575965f8b62cec
Zac Colley <hi@zaccolley.com>
2016-04-04 14:34:04 +0100

    refactor(app): alter visual style of application
    i think having square corners looks snazzier

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,7 +6,7 @@ export default class Header extends Component {
   render() {
     return (
       <header class={style.header}>
-        <h1>bev.space</h1>
+        <h1>bevspace</h1>
         <nav>
           <Link href="/">Home</Link>
           <Link href="/recipes">Recipes</Link>
