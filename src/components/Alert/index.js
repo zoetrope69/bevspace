280aa7a0290750533d8265c394ad1ddd16953ae3
Zac Colley <hi@zaccolley.com>
2016-04-20 21:34:58 +0100

    refactor(alert): improve alert popup
    now fades in and out nicely

--- src/components/Alert/index.js
+++ src/components/Alert/index.js
@@ -7,13 +7,12 @@ export default class Alert extends Component {
 
     // if the service worker is activated and there isnt any recipes synced
     // hide the alert saying its ready to work offline
-    let alertHidden;
-    if (!serviceWorkerActivated && !recipes.length) {
-      alertHidden = style['alert--hidden'];
-    }
+    const ready = (serviceWorkerActivated || !!recipes.length);
 
     return (
-			<div class={`${style.alert} ${alertHidden}`}>Ready to work offline!</div>
+    <div>
+      {ready && <div class={`${style.alert}`}>Ready to work offline!</div>}
+    </div>
 		);
   }
 }
